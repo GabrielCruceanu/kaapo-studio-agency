90f7cfc4236fee8f3da43dc4137247d94c7aacda
Kaapo.Studio <contact@kaapo.studio>
2023-11-15 07:24:26 +0200

    https://bayone-nextjs.themescamp.com source script remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 //= Global Styles
 import "@/src/styles/globals.css";
-import {Metadata} from "next";
 import React from "react";
 import Script from "next/script";
 import {i18n, Locale} from "@/i18n.config";
@@ -17,15 +16,15 @@ export default function Root({
     return (<html lang={params.lang}>
     <body>
     {children}
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/plugins.js" strategy="beforeInteractive"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/wow.min.js" strategy="beforeInteractive"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/gsap.min.js" strategy="beforeInteractive"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/ScrollSmoother.min.js" strategy="beforeInteractive"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/ScrollTrigger.min.js" strategy="beforeInteractive"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/smoother-script.js" strategy="lazyOnload"/>
-    <Script src="https://bayone-nextjs.themescamp.com/assets/js/scripts.js" strategy="lazyOnload"/>
+    <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive"/>
+    <Script src="/assets/js/plugins.js" strategy="beforeInteractive"/>
+    <Script src="/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive"/>
+    <Script src="/assets/js/wow.min.js" strategy="beforeInteractive"/>
+    <Script src="/assets/js/gsap.min.js" strategy="beforeInteractive"/>
+    <Script src="/assets/js/ScrollSmoother.min.js" strategy="beforeInteractive"/>
+    <Script src="/assets/js/ScrollTrigger.min.js" strategy="beforeInteractive"/>
+    <Script src="/assets/js/smoother-script.js" strategy="lazyOnload"/>
+    <Script src="/assets/js/scripts.js" strategy="lazyOnload"/>
     </body>
     </html>)
 }
