ca0f7e1f2cd8e884e010808dda77e9f5526e4de8
Kaapo.Studio <contact@kaapo.studio>
2023-11-15 08:02:04 +0200

    Imports fixed

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -5,19 +5,14 @@ import React from 'react';
 import Script from 'next/script';
 import { i18n, Locale } from '@/i18n.config';
 import Head from 'next/head';
+import generateStylesheetObject from '@/src/common/generateStylesheetsObject';
 
 export const metadata: Metadata = {
     title: 'Kaapo Studio',
     keywords: ['Web design', 'Web development', 'Seo'],
-    authors: [{ url: 'https://kaapo.studio', name: 'Kaapo Studio' }], // siteName: 'Kaapo Studio',
-    // defaultTitle: 'Kaapo Studio',
-    // titleTemplate: ' %s | Kaapo Studio',
+    authors: [{ url: 'https://kaapo.studio', name: 'Kaapo Studio' }],
     description:
         'With over 7 years of experience in web and mobile app design, project strategy, web development, performance optimization, and maintenance & support, we are here to bring your digital vision to life.', // email: 'contact@kaapo.studio',
-    icons: {
-        icon: '/assets/imgs/favicon.ico',
-        shortcut: '/assets/imgs/favicon.ico',
-    },
     metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || process.env.VERCEL_URL || 'http://localhost:3000'),
 };
 
@@ -29,6 +24,12 @@ export default function Root({ children, params }: { children: React.ReactNode; 
     return (
         <html lang={params.lang}>
             <Head>
+                <link rel="icon" href="/assets/imgs/favicon.ico" />
+                <link rel="shortcut icon" href="/assets/imgs/favicon.ico" />
+                <link rel="stylesheet" href="/assets/fonts/mona-sans/style.css" />
+                <link rel="stylesheet" href="/assets/css/plugins.css" />
+                <link rel="stylesheet" href="/assets/css/style.css" />
+                <link rel="stylesheet" href="/landing-preview/css/preview-style.css" />
                 <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
                 <Script src="/assets/js/plugins.js" strategy="beforeInteractive" />
                 <Script src="/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive" />
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,63 +1,49 @@
-import Script from "next/script";
+import Script from 'next/script';
 //= Scripts
 import generateStylesheetObject from '@/src/common/generateStylesheetsObject';
 //= Common Components
-import LoadingScreen from "@/src/components/Common/Loader";
-import Cursor from "@/src/components/Common/Cursor";
-import ProgressScroll from "@/src/components/Common/ProgressScroll";
+import LoadingScreen from '@/src/components/Common/Loader';
+import Cursor from '@/src/components/Common/Cursor';
+import ProgressScroll from '@/src/components/Common/ProgressScroll';
 //= Page Components
-import Navigation from "@/src/components/Preview/Navigation";
-import Header from "@/src/components/Preview/Header";
-import Portfolio from "@/src/components/Preview/Portfolio";
-import Footer from "@/src/components/Preview/Footer";
-import HeaderLocale from "@/src/components/Common/Header-Locale";
-import {Locale} from "@/i18n.config";
-import {getDictionary} from "@/src/lib/dictionary";
+import Navigation from '@/src/components/Preview/Navigation';
+import Header from '@/src/components/Preview/Header';
+import Portfolio from '@/src/components/Preview/Portfolio';
+import Footer from '@/src/components/Preview/Footer';
+import HeaderLocale from '@/src/components/Common/Header-Locale';
+import { Locale } from '@/i18n.config';
+import { getDictionary } from '@/src/lib/dictionary';
 
 export const metadata = {
-  title: 'Kaapo Studio',
-  icons: {
-    icon: "/assets/imgs/favicon.ico",
-    shortcut: "/assets/imgs/favicon.ico",
-    other: generateStylesheetObject([
-      '/assets/fonts/mona-sans/style.css',
-      '/assets/css/plugins.css',
-      '/assets/css/style.css',
-      '/landing-preview/css/preview-style.css'
-    ])
-  }
-}
+    title: 'Kaapo Studio',
+};
 
-export default async function IndexPage({
-                                            params: { lang },
-                                        }: {
-    params: { lang: Locale }
-}) {
-    const { page } = await getDictionary(lang)
-    console.log('page', page)
-  return (
-      <section className='py-24'>
-          <HeaderLocale lang={lang}/>
-          <div className='container'>
-              <h1 className='text-3xl font-bold'>{page.home.title}</h1>
-              <p className='text-gray-500'>{page.home.description}</p>
-          </div>
-      </section>
-    // <body className="main-bg">
-    //   <LoadingScreen />
-    //   <Cursor />
-    //   <ProgressScroll />
-    //
-    //   <div id="smooth-wrapper">
-    //     <div id="smooth-content">
-    //       <Navigation />
-    //       <main>
-    //         <Header />
-    //         <Portfolio />
-    //       </main>
-    //       <Footer />
-    //     </div>
-    //   </div>
-    // </body>
-  )
+export default async function IndexPage({ params: { lang } }: { params: { lang: Locale } }) {
+    const { page } = await getDictionary(lang);
+    console.log('page', page);
+    return (
+        <section className="py-24">
+            <HeaderLocale lang={lang} />
+            <div className="container">
+                <h1 className="text-3xl font-bold">{page.home.title}</h1>
+                <p className="text-gray-500">{page.home.description}</p>
+            </div>
+        </section>
+        // <body className="main-bg">
+        //   <LoadingScreen />
+        //   <Cursor />
+        //   <ProgressScroll />
+        //
+        //   <div id="smooth-wrapper">
+        //     <div id="smooth-content">
+        //       <Navigation />
+        //       <main>
+        //         <Header />
+        //         <Portfolio />
+        //       </main>
+        //       <Footer />
+        //     </div>
+        //   </div>
+        // </body>
+    );
 }
