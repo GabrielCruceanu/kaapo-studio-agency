1c600ff60b2b5551fc3f3e69431562492dd43afb
Kaapo.Studio <contact@kaapo.studio>
2023-11-15 15:08:19 +0200

    imports fixed

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,21 +1,43 @@
-import { Locale } from '@/i18n.config'
-import { getDictionary } from '@/src/lib/dictionary'
-import HeaderLocale from "@/src/components/Common/Header-Locale";
+import React from 'react';
+//= Scripts
+import { Locale } from '@/i18n.config';
+import { getDictionary } from '@/src/lib/dictionary';
+//= Common Components
+import Cursor from '@/src/components/Common/Cursor';
+import ProgressScroll from '@/src/components/Common/ProgressScroll';
+import Navbar from '@/src/components/Common/Navbar';
+import Menu from '@/src/components/Common/Menu';
+import Footer from '@/src/components/Common/Footer';
+//= Page Components
 
-export default async function About({
-                                        params: { lang }
-                                    }: {
-    params: { lang: Locale }
-}) {
-    const { page } = await getDictionary(lang)
+export const metadata = {
+    title: 'About | Kaapo Studio',
+};
 
+export default async function About({ params: { lang } }: { params: { lang: Locale } }) {
+    const { page, navigation, footer } = await getDictionary(lang);
     return (
-        <section className='py-24'>
-            <HeaderLocale lang={lang}/>
-            <div className='container'>
-                <h1 className='text-3xl font-bold'>{page.about.title}</h1>
-                <p className='text-gray-500'>{page.about.description}</p>
+        <>
+            <Cursor />
+            <ProgressScroll />
+
+            <div id="smooth-wrapper">
+                <Navbar borderBottom={false} lang={lang} />
+                <Menu lang={lang} navigation={navigation} />
+                <div id="smooth-content">
+                    <main className="main-bg">
+                        <div className="main-box main-bg onto min-vh-100">
+                            <section className="py-24">
+                                <div className="container">
+                                    <h1 className="text-3xl font-bold">{page.about.title}</h1>
+                                    <p className="text-gray-500">{page.about.description}</p>
+                                </div>
+                            </section>
+                        </div>
+                    </main>
+                    <Footer subBg={true} footer={footer} />
+                </div>
             </div>
-        </section>
-    )
+        </>
+    );
 }
