250e0ed1e6f8b492a17687a7d9de4d4ac31efe0d
Kaapo.Studio <contact@kaapo.studio>
2023-11-15 07:42:08 +0200

    all pages moved;

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,32 +1,42 @@
 //= Global Styles
-import "@/src/styles/globals.css";
-import {Metadata} from "next";
-import React from "react";
-import Script from "next/script";
-import {i18n, Locale} from "@/i18n.config";
+import '@/src/styles/globals.css';
+import { Metadata } from 'next';
+import React from 'react';
+import Script from 'next/script';
+import { i18n, Locale } from '@/i18n.config';
 
 export const metadata: Metadata = {
     title: 'Kaapo Studio',
     keywords: ['Web design', 'Web development', 'Seo'],
-    authors: [{url: 'https://kaapo.studio', name: 'Kaapo Studio'}], // siteName: 'Kaapo Studio',
+    authors: [{ url: 'https://kaapo.studio', name: 'Kaapo Studio' }], // siteName: 'Kaapo Studio',
     // defaultTitle: 'Kaapo Studio',
     // titleTemplate: ' %s | Kaapo Studio',
-    description: 'With over 7 years of experience in web and mobile app design, project strategy, web development, performance optimization, and maintenance & support, we are here to bring your digital vision to life.', // email: 'contact@kaapo.studio',
+    description:
+        'With over 7 years of experience in web and mobile app design, project strategy, web development, performance optimization, and maintenance & support, we are here to bring your digital vision to life.', // email: 'contact@kaapo.studio',
     icons: {
-        icon: "/assets/imgs/favicon.ico", shortcut: "/assets/imgs/favicon.ico"
+        icon: '/assets/imgs/favicon.ico',
+        shortcut: '/assets/imgs/favicon.ico',
     },
     metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || process.env.VERCEL_URL || 'http://localhost:3000'),
-}
+};
 
 export async function generateStaticParams() {
-    return i18n.locales.map(locale => ({lang: locale}))
+    return i18n.locales.map((locale) => ({ lang: locale }));
 }
 
-export default function Root({
-                                 children, params,
-                             }: {
-    children: React.ReactNode
-    params: { lang: Locale }
-}) {
-    return (<>{children}</>)
+export default function Root({ children, params }: { children: React.ReactNode; params: { lang: Locale } }) {
+    return (
+        <html lang={params.lang}>
+            <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/plugins.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/wow.min.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/gsap.min.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/ScrollSmoother.min.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/ScrollTrigger.min.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/smoother-script.js" strategy="lazyOnload" />
+            <Script src="/assets/js/scripts.js" strategy="lazyOnload" />
+            <body>{children}</body>
+        </html>
+    );
 }
---
+++ theme-pages/pages/(portfolio)/portfolio-grid-classic/page.js
@@ -0,0 +1,59 @@
+import Script from "next/script";
+//= Scripts
+import generateStylesheetObject from '../../../../src/common/generateStylesheetsObject';
+//= Common Components
+import LoadingScreen from "../../../../src/components/Common/Loader";
+import Cursor from "../../../../src/components/Common/Cursor";
+import ProgressScroll from "../../../../src/components/Common/ProgressScroll";
+//= Page Components
+import Navbar from "../../../../src/components/Common/Navbar";
+import Menu from "../../../../src/components/Common/Menu";
+import Header from "../../../../src/components/Portfolio/Header";
+import GridClassic from "../../../../src/components/Portfolio/GridClassic";
+import Footer1 from "../../../../src/components/Common/Footer1";
+
+export const metadata = {
+  title: 'Bayone - Portfolio Grid Classic',
+  icons: {
+    icon: "/assets/imgs/favicon.ico",
+    shortcut: "/assets/imgs/favicon.ico",
+    other: generateStylesheetObject([
+      'https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap',
+      '/assets/fonts/mona-sans/style.css',
+      '/assets/css/plugins.css',
+      '/assets/css/style.css'
+    ])
+  }
+}
+
+export default function PortfolioGridClassicPage() {
+  return (
+    <body>
+      <LoadingScreen />
+      <Cursor />
+      <ProgressScroll />
+
+      <Navbar />
+      <Menu />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <main className="main-bg">
+            <Header />
+            <GridClassic />
+          </main>
+          <Footer1 />
+        </div>
+      </div>
+
+
+      <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
+      <Script src="/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive" />
+      <Script src="/assets/js/plugins.js" strategy="beforeInteractive" />
+      <Script src="/assets/js/gsap.min.js" strategy="beforeInteractive" />
+      <Script src="/assets/js/ScrollSmoother.min.js" strategy="beforeInteractive" />
+      <Script src="/assets/js/ScrollTrigger.min.js" strategy="beforeInteractive" />
+      <Script src="/assets/js/smoother-script.js" strategy="lazyOnload" />
+      <Script src="/assets/js/scripts.js" strategy="lazyOnload" />
+    </body>
+  )
+}
