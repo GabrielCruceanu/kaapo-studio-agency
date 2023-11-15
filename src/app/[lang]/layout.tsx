8c69dbc035177f9aaf71832b5fe3b852b5008b51
Kaapo.Studio <contact@kaapo.studio>
2023-11-15 07:48:40 +0200

    All scrips added tot Head

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 import React from 'react';
 import Script from 'next/script';
 import { i18n, Locale } from '@/i18n.config';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
     title: 'Kaapo Studio',
@@ -27,15 +28,17 @@ export async function generateStaticParams() {
 export default function Root({ children, params }: { children: React.ReactNode; params: { lang: Locale } }) {
     return (
         <html lang={params.lang}>
-            <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
-            <Script src="/assets/js/plugins.js" strategy="beforeInteractive" />
-            <Script src="/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive" />
-            <Script src="/assets/js/wow.min.js" strategy="beforeInteractive" />
-            <Script src="/assets/js/gsap.min.js" strategy="beforeInteractive" />
-            <Script src="/assets/js/ScrollSmoother.min.js" strategy="beforeInteractive" />
-            <Script src="/assets/js/ScrollTrigger.min.js" strategy="beforeInteractive" />
-            <Script src="/assets/js/smoother-script.js" strategy="lazyOnload" />
-            <Script src="/assets/js/scripts.js" strategy="lazyOnload" />
+            <Head>
+                <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
+                <Script src="/assets/js/plugins.js" strategy="beforeInteractive" />
+                <Script src="/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive" />
+                <Script src="/assets/js/wow.min.js" strategy="beforeInteractive" />
+                <Script src="/assets/js/gsap.min.js" strategy="beforeInteractive" />
+                <Script src="/assets/js/ScrollSmoother.min.js" strategy="beforeInteractive" />
+                <Script src="/assets/js/ScrollTrigger.min.js" strategy="beforeInteractive" />
+                <Script src="/assets/js/smoother-script.js" strategy="lazyOnload" />
+                <Script src="/assets/js/scripts.js" strategy="lazyOnload" />
+            </Head>
             <body>{children}</body>
         </html>
     );
