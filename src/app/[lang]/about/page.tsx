3b994dd0e2516889536ed3fdb733bb2837b724ef
Kaapo.Studio <contact@kaapo.studio>
2023-11-17 09:05:57 +0200

    about description deleted

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -30,7 +30,6 @@ export default async function About({ params: { lang } }: { params: { lang: Loca
                             <section className="py-24">
                                 <div className="container">
                                     <h1 className="text-3xl font-bold">{page.about.title}</h1>
-                                    <p className="text-gray-500">{page.about.description}</p>
                                 </div>
                             </section>
                         </div>
