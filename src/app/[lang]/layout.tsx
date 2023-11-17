4a5a7a269413942643eadbe230fa348343735d89
Kaapo.Studio <contact@kaapo.studio>
2023-11-17 10:02:29 +0200

    icons added

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -7,6 +7,8 @@ import Script from 'next/script';
 import { i18n, Locale } from '@/i18n.config';
 import generateStylesheetObject from '@/src/common/generateStylesheetsObject';
 import { Nunito_Sans } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
     title: 'Kaapo Studio',
@@ -16,8 +18,10 @@ export const metadata: Metadata = {
         'With over 7 years of experience in web and mobile app design, project strategy, web development, performance optimization, and maintenance & support, we are here to bring your digital vision to life.', // email: 'contact@kaapo.studio',
     metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || process.env.VERCEL_URL || 'http://localhost:3000'),
     icons: {
-        icon: '/assets/imgs/favicon.ico',
-        shortcut: '/assets/imgs/favicon.ico',
+        icon: '/assets/favicon/favicon.ico',
+        shortcut: '/assets/favicon/favicon.ico',
+        apple: '/assets/favicon/apple-touch-icon.png',
+
         other: generateStylesheetObject(['/assets/css/plugins.css']),
     },
 };
@@ -34,6 +38,14 @@ const nunitoSans = Nunito_Sans({
 export default function Root({ children, params }: { children: React.ReactNode; params: { lang: Locale } }) {
     return (
         <html lang={params.lang}>
+            <Head>
+                <meta name="theme-color" content="#F2F2F2" />
+                <link rel="apple-touch-icon" sizes="180x180" href="" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+                <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+                {/*<link rel="alternate" type="application/rss+xml" href="/assets/feed/blog/feed.xml" />*/}
+            </Head>
             <body className={nunitoSans.variable + ' main-bg'}>
                 {children}
                 <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
@@ -45,6 +57,7 @@ export default function Root({ children, params }: { children: React.ReactNode; 
                 <Script src="/assets/js/ScrollTrigger.min.js" strategy="beforeInteractive" />
                 <Script src="/assets/js/smoother-script.js" strategy="lazyOnload" />
                 <Script src="/assets/js/scripts.js" strategy="lazyOnload" />
+                <Analytics />
             </body>
         </html>
     );
